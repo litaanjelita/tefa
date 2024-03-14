e302f9224e6db133b47fb88a4276b2ec0aa670bc
lita anjelita <141709031+litaanjelita@users.noreply.github.com>
2024-03-14 14:42:43 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container-fluid">
+    <div class="row my-5 justify-content-around">
+      <div class="col-lg-5">
+        <nuxt-link to="/pengunjung/tambah">
+          <div class="card bg-pengunjung rounded-5">
+            <div class="card-body">
+            <h2 style="font-family: ;">pengunjung</h2>
+            </div>
+          </div>
+        </nuxt-link>
+  </div>
+
+  <div class="col-lg-5">
+    <nuxt-link to="/buku">
+      <div class="card bg-buku rounded-5">
+        <div class="card-body">
+          <img src="~/assets/img/pencarian.png" alt="logo">
+          <h2 style="font-family: ;">cari bg-buku</h2>
+        </div>
+      </div>
+    </nuxt-link>
+  </div>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  height: 250px;
+  box-shadow: 1px 1px 1px #424242;
+}
+.card.bg-pengunjung {
+  background-image: url('../assets/img/bg-home-kunjungan.jpeg');
+  background-repeat: no-repeat;
+  background-position:center center;
+  background-size: cover;
+}
+.card.bg-buku {
+  background: url('../assets/img/bg-home-cari-buku7,jpeg') no-repeat center center;
+  background-size: cover;
+}
+</style>
---
+++ pages/pengunjung/tambah.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center-my-4">isi buku kunjungan</h2>
+        <form>
+          <div class="mb-3">
+            <input type="text" class="form-control-lg form-select rounded-5" placeholder="NAMA...">
+          </div>
+          <div class="mb-3">
+            <select class="form-control form-control form-select rounded-5">
+              <option value="">KATEGORI</option>
+              <option value="siswa">siswa</option>
+              <option value="guru">guru</option>
+              <option value="staff">staff</option>
+              <option value="umum">umum</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <select class="form-control form-control form-select rounded-5">
+              <option value="">KELAS</option>
+              <option value="1">X</option>
+              <option value="2">XI</option>
+              <option value="3">XII</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <select class="form-control form-control form-select rounded-5">
+              <option value="">KEPERLUAN</option>
+              <option value="baca">baca</option>
+              <option value="pinjam">pinjam</option>
+              <option value="kembalikan">kembalikan buku</option>
+            </select>
+          </div>
+          <nuxt-link to="../pengunjung"><button type="submit" class="btn btn-primary btn-lg rounded-5 px-5">KIRIM</button></nuxt-link>
+          <nuxt-link to="../"><button type="submit" class="btn btn-secondary btn-lg rounded-5 px-5" style="margin-left: 82.5%;">KEMBALI</button></nuxt-link>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
